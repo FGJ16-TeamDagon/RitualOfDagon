50cdeb4de2e09fafd42c13b3ec57516168818afd
Lauri <laurileponiemi@gmail.com>
2016-01-30 22:36:49 +0200

    Base for credits, button to go to main menu from game

--- Assets/Scripts/UI/StartMenu.cs
+++ Assets/Scripts/UI/StartMenu.cs
@@ -3,8 +3,25 @@ using System.Collections;
 
 public class StartMenu : MonoBehaviour
 {
+    public GameObject creditsCanvas;
+
     public void StartGame()
     {
         AppManager.Instance.StartLevel();
+    }
+
+    public void OpenCredits()
+    {
+        creditsCanvas.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        creditsCanvas.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        AppManager.Instance.GoToStartMenu();
     }
 }
