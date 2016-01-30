563f7a968e5abb1ca5ad2d03454c5f1d1c5e3127
Lauri <laurileponiemi@gmail.com>
2016-01-30 23:23:00 +0200

    Main Menu, Credits, Instructions

--- Assets/Scripts/UI/StartMenu.cs
+++ Assets/Scripts/UI/StartMenu.cs
@@ -4,10 +4,16 @@ using System.Collections;
 public class StartMenu : MonoBehaviour
 {
     public GameObject creditsCanvas;
+    public GameObject instructionsCanvas;
 
     public void StartGame()
     {
         AppManager.Instance.StartLevel();
+    }
+
+    public void OpenInstructions()
+    {
+        instructionsCanvas.SetActive(true);
     }
 
     public void OpenCredits()
