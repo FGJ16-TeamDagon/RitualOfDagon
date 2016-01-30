c33439243e7e6a9dfc569679d2723862bf651eca
Timo Moisio <timohmoisio@gmail.com>
2016-01-30 12:31:19 +0200

    Add core gameplay classes

---
+++ Assets/Scripts/Gameplay/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player
+{
+    public string Name { get; protected set; }
+
+    public List<GameCharacter> characters = new List<GameCharacter>();
+
+    public Player(string name)
+    {
+        this.Name = name;
+    }
+}
