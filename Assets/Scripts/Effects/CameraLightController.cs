37d559c62ba893ba86295c10c93134b9234ee49f
Timo Moisio <timohmoisio@gmail.com>
2016-01-31 09:35:46 +0200

    Change lighting settings

--- Assets/Scripts/Effects/CameraLightController.cs
+++ Assets/Scripts/Effects/CameraLightController.cs
@@ -9,14 +9,33 @@ public class CameraLightController : MonoBehaviour
     [SerializeField]
     private float height;
 
+    private float lightRange = 6;
+    private float lightIntensity = 4;
+    private float ambientIntensity = 1f;
+
     private Transform followTarget;
 
     private Transform follower;
+
+    private Light targetLight;
 
     private void Start()
     {
         follower = transform;
         followTarget = Camera.main.transform;
+
+        targetLight = GetComponent<Light>();
+
+        targetLight.intensity = 0;
+        targetLight.range = 0;
+        RenderSettings.ambientIntensity = 0;
+    }
+
+    void Update()
+    {
+        targetLight.intensity = Mathf.MoveTowards(targetLight.intensity, lightIntensity, Time.deltaTime * 2);
+        targetLight.range = Mathf.MoveTowards(targetLight.range, lightRange, Time.deltaTime * 2);
+        RenderSettings.ambientIntensity = Mathf.MoveTowards(RenderSettings.ambientIntensity, ambientIntensity, Time.deltaTime * 2);
     }
 
     private void LateUpdate()
