b0165f3332a811f8a36e48c9c44862d974826d57
Timo Moisio <timohmoisio@gmail.com>
2016-01-31 09:06:30 +0200

    Change level backgrounds

--- Assets/Scripts/Gameplay/GameCameraController.cs
+++ Assets/Scripts/Gameplay/GameCameraController.cs
@@ -10,6 +10,10 @@ public class GameCameraController : MonoBehaviour
     private Transform mover;
     [SerializeField]
     private float damping = 2;
+    [SerializeField]
+    private float targetHeight = 7;
+
+    private float maxMovement = 7;
 
     private Vector3 lastMousePos;
 
@@ -17,6 +21,8 @@ public class GameCameraController : MonoBehaviour
 
     void Update()
     {
+        var moverPos = mover.position;
+
         if (Input.GetMouseButtonDown(0))
         {
             lastMousePos = Input.mousePosition;
@@ -27,16 +33,22 @@ public class GameCameraController : MonoBehaviour
 
             delta /= Screen.height;
 
-            mover.position -= Vector3.forward * delta.y * speed + Vector3.right * delta.x * speed;
+            moverPos -= Vector3.forward * delta.y * speed + Vector3.right * delta.x * speed;
 
             momentum = delta;
         }
         else if (momentum.sqrMagnitude > 0)
         {
-            mover.position -= Vector3.forward * momentum.y * speed + Vector3.right * momentum.x * speed;
+            moverPos -= Vector3.forward * momentum.y * speed + Vector3.right * momentum.x * speed;
 
             momentum = Vector3.MoveTowards(momentum, Vector3.zero, Time.deltaTime * damping);
         }
+
+        moverPos.x = Mathf.Clamp(moverPos.x, -maxMovement, maxMovement);
+        moverPos.y = Mathf.MoveTowards(moverPos.y, targetHeight, Time.deltaTime);
+        moverPos.z = Mathf.Clamp(moverPos.z, -maxMovement, maxMovement);
+
+        mover.position = moverPos;
 
         lastMousePos = Input.mousePosition;
     }
